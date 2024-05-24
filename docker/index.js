30e474d5560d554004582d7b0383585c88aa56e8
Brahmaji Rao <satyamsetty.eswara@gmail.com>
2024-05-24 22:58:45 +0530

    Add dependson for mongodb
    This will ensuer mongo is always up before node app

--- docker/index.js
+++ docker/index.js
@@ -2,12 +2,19 @@ const express = require("express");
 const mongoose = require('mongoose');
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
 
- const app = express();
+const app = express();
 
+const connectWithRetry = () => {
  const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
  mongoose.connect(mongoURL)
  .then(()=> console.log("successfully connected to db"))
- .catch((e) => console.log(e));
+ .catch((e) => {
+   console.log(e);
+   setTimeout(connectWithRetry, 5000);
+ });
+}
+
+connectWithRetry();
 
  app.get("/", (req,res) => {
     res.send("<h1> hello rao garu! </h1>");
